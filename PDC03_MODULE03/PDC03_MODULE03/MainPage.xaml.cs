0476718105ca7d70354ecc11bb00ba1fb8c19413
Arnold Lim <arnoldlim0337@gmail.com>
2023-02-10 14:19:43 +0800

    First Commit

---
+++ PDC03_MODULE03/PDC03_MODULE03/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC03_MODULE03
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
